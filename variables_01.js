00b19929a03adcab6aeedc88b5c21910b5756419
Tanmay <tanmayaswale4@gmail.com>
2023-11-02 23:59:12 +0530

    Variables in Javascript has been done in next step

---
+++ variables_01.js
@@ -0,0 +1,12 @@
+console.log("Variables are like Containers in JavaScript")
+// Variables hold values, and they can be used to store data temporarily or permanently within a program.
+console.log("Dyanamic Type Langugage")
+
+let a = 46
+let b = "Hello"
+let c = true
+console.log(a)
+console.log(b)
+console.log(c)
+a = "Tanmay Aswale"
+console.log(a)
