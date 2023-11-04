f30df225aee3bba95699556021997859dbe2cc02
Tanmay <tanmayaswale4@gmail.com>
2023-11-04 20:01:04 +0530

    Prompt error has been solved

---
+++ condition_Expression_06.js
@@ -0,0 +1,7 @@
+// Different types of Condition Expression in Javascript
+
+const prompt = require('prompt-sync')();
+let a = prompt("What is Your Age?");
+console.log(a);
+console.log(typeof a);
+console.log(a);
